88324b7ef2ba626f7bfe3275cb1b48288303f2f6
davide vanoncini <davidevanoncini2003@gmail.com>
2021-12-30 22:56:56 +0100

    changed from gin to mux

--- endpoints.go
+++ endpoints.go
@@ -1,13 +1,16 @@
 package main
 
 const (
+	//git endpoints
+	GitRepo string = "/git/api/{user}/{repo}/"
+
 	//user endpoints
-	NewUser    string = "/api/newUser/:user"
-	DeleteUser string = "/api/deleteUser/:user"
+	NewUser    string = "/git/api/newUser/{user}"
+	DeleteUser string = "/git/api/deleteUser/{user}"
 
 	//repo endpoints
-	GetRepos   string = "/api/:user/repos"
-	GetRepo    string = "/api/:user/:repo"
-	AddRepo    string = "/api/:user/add/:repo"
-	DeleteRepo string = "/api/:user/delete/:repo"
+	GetRepos   string = "/git/api/{user}/repos"
+	GetRepo    string = "/git/api/{user}/{repo}/get"
+	AddRepo    string = "/git/api/{user}/add/{repo}"
+	DeleteRepo string = "/git/api/{user}/delete/{repo}"
 )
