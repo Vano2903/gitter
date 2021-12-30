c749ba0a4e2a939e335ff2f03588859f83817339
davide vanoncini <davidevanoncini2003@gmail.com>
2021-12-31 00:05:42 +0100

    changed way repo get generated

--- main.go
+++ main.go
@@ -10,8 +10,30 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc(GitRepoRecivePack, GitHandler.ServeHTTP)
-	r.HandleFunc(GitRepoInfo, GitHandler.ServeHTTP)
+	r.HandleFunc(GitRepoRecivePack, func(w http.ResponseWriter, r *http.Request) {
+		user := mux.Vars(r)["user"]
+		repo := mux.Vars(r)["repo"]
+
+		// user, pass, ok := r.BasicAuth()
+		// if !ok {
+		// 	// return cred, fmt.Errorf("authentication failed")
+		// 	fmt.Fprintf(w, "authentication failed")
+		// }
+		r.URL.Path = user + "/" + repo
+		GitHandler.ServeHTTP(w, r)
+	})
+	r.HandleFunc(GitRepoRecivePack, func(w http.ResponseWriter, r *http.Request) {
+		user := mux.Vars(r)["user"]
+		repo := mux.Vars(r)["repo"]
+
+		// user, pass, ok := r.BasicAuth()
+		// if !ok {
+		// 	// return cred, fmt.Errorf("authentication failed")
+		// 	fmt.Fprintf(w, "authentication failed")
+		// }
+		r.URL.Path = user + "/" + repo
+		GitHandler.ServeHTTP(w, r)
+	})
 	fmt.Println(conf.Port)
 	log.Fatal(http.ListenAndServe(":"+conf.Port, r))
 	log.Println("partito")
