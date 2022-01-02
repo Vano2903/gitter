e5be0a3bd567b91ac341c473982a71dbda471fbf
davide vanoncini <davidevanoncini2003@gmail.com>
2022-01-02 04:19:18 +0100

    added function to create a new repo and run git init

--- git.go
+++ git.go
@@ -1,20 +1,31 @@
 package main
 
-import "github.com/sosedoff/gitkit"
+import (
+	"os"
+	"os/exec"
+
+	"github.com/sosedoff/gitkit"
+)
 
 func NewServer(repoDir string) *gitkit.Server {
-	// Configure git hooks
-	// hooks := &gitkit.HookScripts{
-	// 	PreReceive: `echo "Thanks from Gitter! :D"`,
-	// }
-
 	// Configure git service
 	server := gitkit.New(gitkit.Config{
 		Dir:        repoDir,
 		AutoCreate: true,
-		//AutoHooks:  true,
-		//Hooks:      hooks,
 	})
 
 	return server
+}
+
+func CreateNewDir(path string, gitInit bool) error {
+	// create new directory
+	if err := os.Mkdir(path, 0755); err != nil {
+		return err
+	}
+	//run git init --bare command
+	if gitInit {
+		cmd := exec.Command("git", "init", "--bare", "--quiet", path)
+		return cmd.Run()
+	}
+	return nil
 }
